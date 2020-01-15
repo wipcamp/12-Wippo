c9f1a47ac52d35bb3e507b52bf2daf0b244a4c0b
pipe34150 <pipe34150@hotmail.com>
2020-01-15 14:49:24 +0700

    [ADD] Core/Header.js

---
+++ src/components/Core/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Layout } from 'antd'
+
+const { Header } = Layout;
+
+export default class MainHeader extends Component {
+  render() {
+    return (
+      <Header>This is Header Ja.</Header>
+    )
+  }
+}
