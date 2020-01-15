be1fe0bc74902442d7bd6564cb4726907c042e92
pipe34150 <pipe34150@hotmail.com>
2020-01-15 13:12:17 +0700

    [UPDATE] Test Sider in Core/index.js

--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Sider from './Sider'
 
 export default class Index extends Component {
   render() {
     return (
       <div>
-        Wippo
+        <Sider />
       </div>
     )
   }
