dced78cc4cc06b5b7e551afca119d446e494b43a
pipe34150 <pipe34150@hotmail.com>
2020-01-15 14:50:34 +0700

    [ADD] Dashboard.js File for graph

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        ดะ ดะ แดชบอร์ด
+      </div>
+    )
+  }
+}
