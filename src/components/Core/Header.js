d37ba49244e6564ca58abc6e80be4bdc64d78f35
pipe34150 <pipe34150@hotmail.com>
2020-01-17 13:13:16 +0700

    [UPDATE] Header.js change class to className

--- src/components/Core/Header.js
+++ src/components/Core/Header.js
@@ -13,8 +13,8 @@ export default class MainHeader extends Component {
   render() {
     return (
       <Header style={{textAlign: 'right',color:'white'}}>
-          {this.state.name} ({this.state.position}) {" "}{" "}
-          <button class="btn ant-btn-danger">Logout</button>
+          {this.state.name} ({this.state.position}) {" "}
+          <button className="btn ant-btn-danger">Logout</button>
       </Header>
     )
   }
