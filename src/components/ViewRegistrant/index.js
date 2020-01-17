92c1a5bf1c4b42fd83d2fc5b4f8ee5064f812183
pipe34150 <pipe34150@hotmail.com>
2020-01-17 13:15:12 +0700

    [UPDATE] ViewRegistrant remove mock up data

--- src/components/ViewRegistrant/index.js
+++ src/components/ViewRegistrant/index.js
@@ -6,20 +6,6 @@ const { TabPane } = Tabs;
 const Menu = [
   'รออนุมัติ' , 'WIPPER ในระบบ'
 ]
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
 
 const columns = [
   {
