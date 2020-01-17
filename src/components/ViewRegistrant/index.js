b7ee429ee3c689e39af3732eae562a894ec8e9e0
pipe34150 <pipe34150@hotmail.com>
2020-01-17 13:13:41 +0700

    [UPDATE] View Registrant with real data

--- src/components/ViewRegistrant/index.js
+++ src/components/ViewRegistrant/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Tabs, Icon , Table } from 'antd';
+import userService from '../../services/UserService';
 
 const { TabPane } = Tabs;
 const Menu = [
@@ -22,23 +23,36 @@ const dataSource = [
 
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
+    title: 'wipId',
+    dataIndex: 'wipId',
+    key: 'wipId',
+  },{
+    title: 'lineId',
+    dataIndex: 'lineId',
+    key:'lineId',
+  },{
+    title: 'firstName',
+    dataIndex: 'firstName',
+    key:'firstName',
+  },{
+    title: 'lastName',
+    dataIndex: 'lastName',
+    key:'lastName',
+  },{
+    title: 'status',
+    dataIndex: 'status',
+    key:'status',
+  }
 ];
 
 export default class index extends Component {
+  state = {
+    userData:null,
+  }
+  async componentDidMount(){
+    let data = await userService.getAllUser()
+    this.setState({userData:data.data.data});
+  }
   render() {
     return (
       <Tabs defaultActiveKey="1">
@@ -53,8 +67,8 @@ export default class index extends Component {
                   </span>
                 }
                 key={i}>
-                  
-                  <Table dataSource={dataSource} columns={columns} />
+                  {console.log(this.state.userData)}
+                  <Table dataSource={this.state.userData} columns={columns} />
                 </TabPane>
             )
           })
