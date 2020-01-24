24725acae99fb43c12a040ed5079d051fb8cc993
pipe34150 <pipe34150@hotmail.com>
2020-01-24 16:21:45 +0700

    [UPDATE] Hourly graph with dropdown calendar date

--- src/components/Dashboard/HourlyGraph.js
+++ src/components/Dashboard/HourlyGraph.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Bar } from 'ant-design-pro/lib/Charts';
 import userService from '../../services/UserService'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export default class HourlyGraph extends Component {
@@ -12,35 +14,45 @@ export default class HourlyGraph extends Component {
 
   componentDidMount(){
     let date = new Date();
-    let year = date.getFullYear();
-    let month = ("0" + (date.getMonth() + 1)).slice(-2);
-    let day = ("0" + date.getDate()).slice(-2);
-    let currentDate = year+"-"+month+"-"+day
-    this.setState({
-      date: currentDate
-    })
-    this.getHourlyGraph(currentDate)
+    this.setDateState(date);
   }
 
   async getHourlyGraph(currentDate){
+    this.setState({
+      userData : []
+    })
     let data = await userService.getHourlyGraph(currentDate);
     let hourly = data.data.data;
-    console.log(hourly)
     for(let i = 0 ; i < 24 ; i++){
       this.state.userData.push({
         x: `${i + 1}:00`,
         y: hourly[i],
       });
     }
+    console.log(currentDate)
+  }
+
+  setDateState(date){
+    let year = date.getFullYear();
+    let month = ("0" + (date.getMonth() + 1)).slice(-2);
+    let day = ("0" + date.getDate()).slice(-2);
+    let currentDate = year+"-"+month+"-"+day
+    this.setState({
+      date: date
+    })
+    this.getHourlyGraph(currentDate)
   }
 
   render() {
     return (
+      <div>
+      <DatePicker selected={this.state.date} onChange={date => this.setDateState(date)} /> 
       <Bar 
         height={200} 
         title="จำนวนคนที่ลงทะเบียนต่อวัน" 
         data={this.state.userData} 
       />
+      </div>
     )
   }
 }
