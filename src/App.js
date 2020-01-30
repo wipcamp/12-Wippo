7bae98e4d550e67e5cd87b7148ed601dcab9d71e
pipe34150 <pipe34150@hotmail.com>
2020-01-30 16:46:39 +0700

    [UPDATE] App.js with main part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Index from '../src/components/Core'
+import Chart from '../src/components/Dashboard'
 
 export default class App extends Component {
   render() {
