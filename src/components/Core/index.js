9fc3ea0622532cb7566646c0466fbe16567c7f80
pipe34150 <pipe34150@hotmail.com>
2020-01-30 17:23:55 +0700

    [UPDATE] Remove sider and component to router.js

--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -3,9 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min'
 import 'bootstrap/dist/js/bootstrap.min'
 
-import Sider from './Sider'
+import MainRouter from '../../Router'
 import Header from './Header'
-import ViewRegistrant from '../ViewRegistrant'
 
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import Dashboard from '../Dashboard'
@@ -15,28 +14,14 @@ const { Content } = Layout;
 export default class Index extends Component {
   render() {
     return (
-      <Layout>
         <Layout>
-          <Header />
-        </Layout>
-        <Layout style={{ minHeight: '100vh' }}>
-          <Sider />
           <Layout>
-            <Content style={{ margin: '16px' }}>
-              {/* <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb> */}
-              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                
-                {/* <button className="btn btn-danger" >5555</button> */}
-                <Dashboard />
-                {/* <ViewRegistrant /> */}
-              </div>
-            </Content>
+            <Header />
+          </Layout>
+          <Layout style={{ minHeight: '100vh' }}>
+            <MainRouter />
           </Layout>
         </Layout>
-      </Layout>
     )
   }
 }
