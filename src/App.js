d44bad7d6a0af0385c2bfec4fbfa1af5604e2978
pipe34150 <pipe34150@hotmail.com>
2020-01-31 13:32:36 +0700

    [UPDATE] remove import in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-
 import Index from '../src/components/Core'
-import Chart from '../src/components/Dashboard'
 
 export default class App extends Component {
   render() {
